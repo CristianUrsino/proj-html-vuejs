b56380701f181ea30fe1e92c33ef3d88cbdae252
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-21 17:06:05 +0100

    added buttons in header

--- src/components/header/HeroComponent.vue
+++ src/components/header/HeroComponent.vue
@@ -33,12 +33,12 @@
             </div>
 
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="carousel-control-prev-icon slide-btn" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+                <span class="carousel-control-next-icon slide-btn" aria-hidden="true"></span>
+                <span class="visually-hidden ">Next</span>
             </button>
         </div>
 
