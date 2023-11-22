9efc2111b1bc7388ba22895aaf1fdbaeea035316
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-22 09:55:52 +0100

    added static footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,99 @@
 <template>
-    
+    <footer class="d-flex">
+
+        <div class="mt-5 pt-5 container-fluid">
+
+            <div class="row justify-content-between wrap">
+                <ul class="col-12 col-md-4">
+                    <li class="title-list mb-3">FIND OUR RESTAURANTS</li>
+                    <li class="mb-3">
+                        <ul>
+                            <div><a href="#">Lorem ipsum dolor.</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                        </ul>
+                    </li>
+                    <li class="mb-3">
+                        <ul>
+                            <div><a href="#">Lorem ipsum dolor.</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                        </ul>
+                    </li>
+                    <li class="mb-3">
+                        <ul>
+                            <div><a href="#">Lorem ipsum dolor.</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                        </ul>
+                    </li>
+                    <li class="mb-3">
+                        <ul>
+                            <div><a href="#">Lorem ipsum dolor.</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                            <div><a href="#">Lorem ipsum dolor..</a></div>
+                        </ul>
+                    </li>
+                </ul>
+
+                <div  class="col-12 col-md-4 d-flex flex-column">
+                    <ul>
+                        <li class="title-list mb-3">WORKING HOURS</li>
+                        <li class="mb-3">
+                            <ul>
+                                <li class="sub-title-list">LOREM</li>
+                                <li class="red-text">Lorem, ipsum.</li>
+                            </ul>
+                        </li>
+                        <li class="mb-3">
+                            <ul>
+                                <li class="sub-title-list">LOREM</li>
+                                <li>Lorem, ipsum.</li>
+                            </ul>
+                        </li>
+                        <li class="mb-3">
+                            <ul>
+                                <li class="sub-title-list">LOREM</li>
+                                <li>Lorem, ipsum.</li>
+                            </ul>
+                        </li>
+                        <li class="mb-3">
+                            <ul>
+                                <li class="sub-title-list">LOREM</li>
+                                <li>Lorem, ipsum.</li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    <div class="d-flex follow-us pb-3">
+                    <div class="title-list">FOLLOW US:</div>
+                    <ul class="d-flex pt-2">
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                    </ul>
+                </div>
+                </div>
+
+                <h3 class="col-12 col-md-4">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis quaerat libero incidunt.
+                </h3>
+
+            </div>
+
+            <div class="d-flex justify-content-between created-by px-4">
+                <div class="align-self-end pb-5"><span class="sub-title-list">created by</span> <span>boolean</span></div>
+                <figure><img src="../assets/images/footer-bottom-img.png" alt="logo"></figure>
+            </div>
+            
+        </div>
+
+        <figure class="d-none d-lg-block">
+            <img src="../assets/images/footer-img-1.jpg" alt="">
+        </figure>
+
+    </footer>
 </template>
 
 <script>
